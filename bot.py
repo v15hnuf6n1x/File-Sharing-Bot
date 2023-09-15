ed775d021ea589ace88ee7e46494534a76cd4584
VISHNU <Vishnuvishva2434@gmail.com>
2023-09-15 22:38:40 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -45,7 +45,7 @@ class Bot(Client):
                 self.LOGGER(__name__).warning(a)
                 self.LOGGER(__name__).warning("Bot can't Export Invite link from Force Sub Channel!")
                 self.LOGGER(__name__).warning(f"Please Double check the FORCE_SUB_CHANNEL value and Make sure Bot is Admin in channel with Invite Users via Link Permission, Current Force Sub Channel Value: {FORCE_SUB_CHANNEL}")
-                self.LOGGER(__name__).info("\nBot Stopped. Join https://t.me/Web_X_bots for support")
+                self.LOGGER(__name__).info("\nthere is an error so Bot is Stopped")
                 sys.exit()
         try:
             db_channel = await self.get_chat(CHANNEL_ID)
@@ -55,12 +55,12 @@ class Bot(Client):
         except Exception as e:
             self.LOGGER(__name__).warning(e)
             self.LOGGER(__name__).warning(f"Make Sure bot is Admin in DB Channel, and Double check the CHANNEL_ID Value, Current Value {CHANNEL_ID}")
-            self.LOGGER(__name__).info("\nBot Stopped. Join https://t.me/Web_X_Bots for support")
+            self.LOGGER(__name__).info("\nthere is an error so Bot is Stopped")
             sys.exit()
 
         self.set_parse_mode(ParseMode.HTML)
         self.LOGGER(__name__).info(f"Bot Running..!\n\nCreated by \nhttps://t.me/CodeXBotz")
-        self.LOGGER(__name__).info(f""" \n\n   ❤️ WebXBots """)
+        self.LOGGER(__name__).info(f""" \n\n   ❤️ Mr_V_bots """)
         self.username = usr_bot_me.username
         #web-response
         app = web.AppRunner(await web_server())
@@ -70,4 +70,4 @@ class Bot(Client):
 
     async def stop(self, *args):
         await super().stop()
-        self.LOGGER(__name__).info("Bot stopped.")
+        self.LOGGER(__name__).info("There is a error so Bot stopped.")
